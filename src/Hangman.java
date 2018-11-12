00ee5c8fd6ef20ac7b8d38004927325e5303e002
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-11-12 08:07:18 -0500

    Picture
    Hangman image complete, touch up any small details to finish ALPHA

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 
 public class Hangman {
 
-    private static String[] words = {"toaster", "dinosaur", "keyboard", "gorilla", "motherboard", "pineapple", "butterfly" };
-    private static String word = words[(int) (Math.random() * words.length)];
+    private static String[] wordList = {"toaster", "dinosaur", "keyboard", "gorilla", "motherboard", "pineapple", "butterfly" };
+    private static String word = wordList[(int) (Math.random() * wordList.length)];
     private static String underscore = new String(new char[word.length()]).replace("\0", "_");
     private static int count = 0;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Welcome to hangman!");
         System.out.println("You will have 15 tries to attempt to win the game.");
 
         while (count < 15 && underscore.contains("_")) {
             System.out.println("Guess a letter in the word.");
-            System.out.println("You will have 15 tries to attempt to win the game.");
             System.out.println(underscore);
-            String guess = sc.next();
+            String guess = keyboard.next();
             hang(guess);
         }
-        sc.close();
+        keyboard.close();
     }
 
     public static void hang(String guess) {
@@ -49,8 +48,220 @@ public class Hangman {
     public static void hangmanPicture() {
         if (count == 1) {
             System.out.println("Wrong guess, try again.");
-            System.out.println("__|__");
+            System.out.println("___|___");
+            System.out.println("You have 14 tries remaining.");
         }
+        if (count == 2) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 13 tries remaining.");
+        }
+        if (count == 3) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 12 tries remaining.");
+        }
+        if (count == 4) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 11 tries remaining.");
+        }
+        if (count == 5) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |          |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 10 tries remaining.");
+        }
+        if (count == 6) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 9 tries remaining.");
+        }
+        if (count == 7) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        |   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 8 tries remaining.");
+        }
+        if (count == 8) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        |   |");
+            System.out.println("   |        |   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 7 tries remaining.");
+        }
+        if (count == 9) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |        | | |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 6 tries remaining.");
+        }
+        if (count == 10) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |        | | |");
+            System.out.println("   |          |");
+            System.out.println("   |          |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 5 tries remaining.");
+        }
+        if (count == 11) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |        | | |");
+            System.out.println("   |          |");
+            System.out.println("   |          |");
+            System.out.println("   |         / \\");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 4 tries remaining.");
+        }
+        if (count == 12) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |        | | |");
+            System.out.println("   |          |");
+            System.out.println("   |          |");
+            System.out.println("   |         / \\");
+            System.out.println("   |        /   \\");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 3 tries remaining.");
+        }
+        if (count == 13) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |        | | |");
+            System.out.println("   |          |");
+            System.out.println("   |          |");
+            System.out.println("   |         / \\");
+            System.out.println("   |       _/   \\_");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 2 tries remaining.");
+        }
+        if (count == 14) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   |         _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |       _| | |_");
+            System.out.println("   |          |");
+            System.out.println("   |          |");
+            System.out.println("   |         / \\");
+            System.out.println("   |       _/   \\_");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("You have 1 try remaining.");
+        }
+        if (count == 15) {
+            System.out.println("Wrong guess, try again.");
+            System.out.println("   ____________");
+            System.out.println("   |          O");
+            System.out.println("   | L       _|_");
+            System.out.println("   |        | | |");
+            System.out.println("   |       _| | |_");
+            System.out.println("   | You Lose |");
+            System.out.println("   |          |");
+            System.out.println("   |         / \\");
+            System.out.println("   |       _/   \\_");
+            System.out.println("   |");
+            System.out.println("___|___");
+            System.out.println("Game over! The word was " + word);
+            System.out.println("Try again to win!");
 
+
+        }
     }
 }
